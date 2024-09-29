36768f30b5496c3265f893fec1300b298e381f11
dhruv2444 <121995293+dhruv2444@users.noreply.github.com>
2024-09-29 13:59:42 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = 32
+
+console.log(typeof score);
+
+valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+/*
+VARIABLES VALUEiNnUMBER    
+null valueInNumber is 0
+    undefined valiInNumber is Nan
+    string "sdsdsad" vaLUE IS Nan
+    string"22" value is 22
+    string"223fdsfds" value is nan
+    boolean value is 0 or 1 
+*/
+
+/*
+    1 true
+    0 false
+    emptystring "" value is false
+    string "sfedge" value is true
+*/ 
+
+/* 
+Boolean(variable)
+String(variable)
+*/
