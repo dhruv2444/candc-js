4b51108abb2769f5fac19c6874848f9f76c13740
dhruv2444 <121995293+dhruv2444@users.noreply.github.com>
2024-09-29 13:47:01 +0000

    data types and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 1
+var accountEmail ="asdasd@"
+let accountPassword = "123456"
+accountCity= "ramnagar"
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict" //treat all js code in file as newer version;
+
+
+console.log(typeof 5);
+console.log(typeof null)
+
+// null type is object******
